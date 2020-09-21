4410860ca7cdaa8bc14ba94616d5ed5ae052561b
Inline2 <69176265+Inline2@users.noreply.github.com>
2020-09-21 05:59:40 -0400

    Added comments and maybe optimized some things idk

--- random/random.ino
+++ random/random.ino
@@ -12,18 +12,18 @@ int amount = 10;
 void setup() {
   serX.attach(10);
   serY.attach(11);
-  Serial.begin(9600);
-  Serial.setTimeout(10);
-  serX.write(90);
+  //Serial.begin(9600);
+  //Serial.setTimeout(10); //I think these are actually useless and I just forgot to take them out so if it doesn't work try uncommenting these
+  serX.write(90); //Makes the servos move to a straight forward position
   serY.write(90);
 }
 
 void loop() {
   //Y100-130 X30-110
+  //The default range I have it set up for
   dir = random(4);
-  amount = 10;
   delay(200);
-  switch(dir)
+  switch(dir) //changing the value of amount will change how much the servos move
   {
     case 0:
       amount = 10;
@@ -42,7 +42,7 @@ void loop() {
       yNegDir();
       break;
   }
-  while(yData > 130 || yData < 100 || xData > 110 || xData < 30)
+  while(yData > 130 || yData < 100 || xData > 110 || xData < 30) //change these and the if elses below to modify the bounds of the servos
   {
     if(yData > 130)
     {
@@ -67,7 +67,7 @@ void loop() {
   }
   
 }
-void xPosDir()
+void xPosDir() //all of these functions just make the servo move more smoothly with less speed.
 {
   amount = xData + amount;
   for(xData = xData; xData <= amount; xData++)
